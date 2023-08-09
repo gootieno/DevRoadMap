362f30102411d012ff8f5ef481d442e9309f111d
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-08-08 17:34:13 -0700

    refactoring app for multi form

--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,180 @@
-import { buildForm, initializePage } from "./containers.js";
-import { clearForm } from "./localStorage.js";
+import { formFieldHeadings } from "./data.js";
+import { saveFormData, getFormData, clearFormData } from "./localstorage.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-  initializePage();
+const formTypes = Object.keys(formFieldHeadings);
+let currentFormType = localStorage.getItem("currentFormType") || formTypes[0];
 
-  const body = document.querySelector("body");
-  const menuContainer = document.querySelector("#menu-container");
+function createFormSection(heading, todos, formData, formType) {
+  const section = document.createElement("div");
+  section.className = "form-section";
 
-  body.addEventListener("click", (e) => {
-    e.stopPropagation();
+  const headingElement = document.createElement("h2");
+  headingElement.textContent = heading;
+  section.appendChild(headingElement);
 
-    if (!menuContainer.className.includes("hide-menu"))
-      menuContainer.classList.add("hide-menu");
-  });
+  todos.forEach((todo) => {
+    const label = document.createElement("label");
+    label.textContent = todo;
 
-  const themeInputs = document.querySelectorAll(".theme-inputs");
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.className = "checkbox";
+    input.checked = formData.includes(todo);
 
-  themeInputs.forEach((themeInput) => {
-    themeInput.addEventListener("input", (e) => {
-      if (e.target.value === "dark") {
-        document.body.classList.add("darkModeOn");
-        document.body.classList.remove("darkModeOff");
-        localStorage.setItem(
-          "theme-settings",
-          JSON.stringify({ darkMode: true, lightMode: false })
-        );
+    const labelContainer = document.createElement("div");
+    labelContainer.className = "label-container";
+    labelContainer.appendChild(input);
+    labelContainer.appendChild(label);
+
+    section.appendChild(labelContainer);
+
+    input.addEventListener("change", () => {
+      if (input.checked) {
+        formData.push(todo);
       } else {
-        document.body.classList.remove("darkModeOn");
-        document.body.classList.add("darkModeOff");
-        localStorage.setItem(
-          "theme-settings",
-          JSON.stringify({ darkMode: false, lightMode: true })
-        );
+        const index = formData.indexOf(todo);
+        if (index !== -1) {
+          formData.splice(index, 1);
+        }
       }
+      saveFormData(formType, formData);
     });
   });
 
-  const clearFormButton = document.getElementById("clear-form");
+  return section;
+}
 
-  clearFormButton.addEventListener("click", (e) => {
-    clearForm("form-storage");
-    // buildForm();
+function renderForm(formType) {
+  const formFields = formFieldHeadings[formType];
+  if (!formFields) return;
+
+  const formContainer = document.getElementById("form-container");
+  removeAllChildren(formContainer);
+
+  const formData = getFormData(formType);
+
+  for (const sectionHeading in formFields) {
+    const todos = formFields[sectionHeading];
+    const formSection = createFormSection(
+      sectionHeading,
+      todos,
+      formData,
+      formType
+    );
+    formContainer.appendChild(formSection);
+  }
+}
+
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+function createButton(text, parent, formType) {
+  const label = document.createElement("label");
+  label.textContent = text;
+
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.name = "formType";
+  input.value = formType;
+  input.addEventListener("change", () => {
+    renderForm(formType);
+    createHeader(formType);
   });
 
-  const menu = document.getElementById("heading-menu");
+  label.appendChild(input);
+  parent.appendChild(label);
+  return input;
+}
 
-  menu.addEventListener("click", (e) => {
-    e.stopPropagation();
-    console.log("cals", menuContainer.className);
-    if (menuContainer.className.includes("hide-menu")) {
-      menuContainer.classList.remove("hide-menu");
+function createHeader(formType) {
+  const header = document.querySelector(".navbar .header");
+  if (header) {
+    header.textContent = formType;
+  }
+
+  const formTypeRadioContainer = document.querySelector(
+    ".form-type-radio-container"
+  );
+  const formTypeButtons = formTypeRadioContainer.querySelectorAll(
+    'input[type="radio"]'
+  );
+  formTypeButtons.forEach((button) => {
+    if (button.value === formType) {
+      button.checked = true;
     } else {
-      menuContainer.classList.add("hide-menu");
+      button.checked = false;
     }
   });
 
-  const formInputs = document.querySelectorAll(".form-inputs");
+  localStorage.setItem("currentFormType", formType); // Save the current form type
+}
 
-  formInputs.forEach((formInput) => {
-    formInput.addEventListener("input", (e) => {
-      const inputName = e.target.dataset.fieldName;
+function createPage() {
+  const navbar = document.createElement("nav");
+  navbar.className = "navbar";
+  document.body.insertBefore(navbar, document.body.firstChild);
 
-      const formLabel = document.querySelector(
-        `label[data-field-name=${inputName}]`
-      );
+  const header = document.createElement("div");
+  header.className = "header";
+  navbar.appendChild(header);
 
-      const formStorage = JSON.parse(localStorage.getItem("form-storage"));
+  const dropdownButton = document.createElement("button");
+  dropdownButton.id = "dropdown-button";
+  dropdownButton.textContent = "Menu";
+  dropdownButton.addEventListener("click", toggleMenu);
+  header.appendChild(dropdownButton);
 
-      if (e.target.checked) {
-        formStorage[e.target.name] = "true";
-        formLabel.style.textDecoration = "line-through";
-        formLabel.style.color = "green";
-        e.target.removeAttribute("checked");
-      } else {
-        e.target.setAttribute("checked", "false");
-        formStorage[e.target.name] = "false";
-        formLabel.style.textDecoration = "none";
-        formLabel.style.color = "inherit";
-      }
+  const heading = document.createElement("h1");
+  heading.textContent = currentFormType;
+  header.appendChild(heading);
 
-      localStorage.setItem("form-storage", JSON.stringify(formStorage));
-    });
+  const formContainer = document.createElement("form");
+  formContainer.id = "form-container";
+  document.body.appendChild(formContainer);
+
+  const formTypeRadioContainer = document.createElement("div");
+  formTypeRadioContainer.className = "form-type-radio-container";
+  formTypes.forEach((formType) => {
+    createButton(formType, formTypeRadioContainer, formType);
   });
-});
+  navbar.appendChild(formTypeRadioContainer);
+
+  // Render the initial form and header
+  renderForm(currentFormType);
+  createHeader(currentFormType);
+}
+
+function toggleMenu() {
+  const dropdownMenu = document.getElementById("dropdown-menu");
+  dropdownMenu.classList.toggle("open");
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+function clearCheckedInputs() {
+  const form = document.querySelector("form");
+  const checkboxes = form.querySelectorAll(".checkbox");
+  const formType = document.querySelector(
+    'input[name="formType"]:checked'
+  ).value;
+  const formData = getFormData(formType);
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      checkbox.checked = false;
+      const index = formData.indexOf(checkbox.nextSibling.textContent);
+      if (index !== -1) {
+        formData.splice(index, 1);
+      }
+    }
+  });
+
+  saveFormData(formType, formData);
+}
+
+window.addEventListener("DOMContentLoaded", createPage);
--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,27 +1,15 @@
-import { buildForm } from "./containers.js";
+// localstorage.js
 
-export const addToLocalStorage = (name) => {
-  localStorage.setItem(JSON.stringify(name), true);
-};
-
-export const buildLocalStorage = (storageName) => {
-  switch (storageName) {
-    case "form-storage":
-      localStorage.setItem("form-storage", JSON.stringify({}));
-      break;
-    case "theme-settings":
-      localStorage.setItem(
-        "theme-settings",
-        JSON.stringify({ darkMode: false, lightMode: false })
-      );
-      break;
-    default:
-      alert(new Error("No storage name"));
+export function saveFormData(formType, formData) {
+    localStorage.setItem(`formData-${formType}`, JSON.stringify(formData));
   }
-};
-
-export const clearForm = (storageName) => {
-  buildLocalStorage(storageName);
-  document.getElementById("form-container").remove();
-  buildForm();
-};
+  
+  export function getFormData(formType) {
+    const formData = localStorage.getItem(`formData-${formType}`);
+    return formData ? JSON.parse(formData) : [];
+  }
+  
+  export function clearFormData(formType) {
+    localStorage.removeItem(`formData-${formType}`);
+  }
+  
